9fff8984c5728d1db3093d011b5215be22f3dba2
Stijn Scheltinga Koopman <stijnskoop@gmail.com>
2024-01-06 18:57:23 +0100

    added 03/00

---
+++ cpp_module_03/ex01/ScavTrap.hpp
@@ -0,0 +1,8 @@
+#include "claptrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+    public:
+        ScavTrap( const std::string& name );
+        ~ScavTrap();
+}
--- cpp_module_03/ex01/main.cpp
+++ cpp_module_03/ex01/main.cpp
@@ -3,12 +3,8 @@
 int main(void)
 {
     ClapTrap random("wessel");
-    ClapTrap random2;
 
-    random2 = random;
-
-    // for (int i = 0; i < 11; i++)
-    //     random.attack("remco");
+    random.attack("remco");
     random.beRepaired(4);
     random.takeDamage(5);
 }
