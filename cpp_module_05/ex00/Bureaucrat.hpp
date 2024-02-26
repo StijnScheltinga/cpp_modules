44b33b82dd1ea2f7ccb6b3daa698d4b329182a37
StijnScheltinga <stijnskoop@gmail.com>
2024-02-26 18:15:34 +0100

    small start on cpp 05

---
+++ cpp_module_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,18 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+
+class Bureaucrat
+{
+    private:
+        const std::string	name;
+		unsigned int		grade;
+	public:
+		Bureaucrat();
+		~Bureaucrat();
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat& operator=(const Bureaucrat& other);
+};
+
+#endif
