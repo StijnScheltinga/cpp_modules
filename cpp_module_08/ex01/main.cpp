af3486c0d465920c3df9b8003b845fb2768db894
Stijn Scheltinga Koopman <stijnskoop@gmail.com>
2024-08-16 14:13:03 +0200

    ex01 is ready for eval

--- cpp_module_08/ex01/main.cpp
+++ cpp_module_08/ex01/main.cpp
@@ -1,77 +1,100 @@
 #include "Span.hpp"
 #include <iostream>
 #include <list>
+#include <cstdlib>
 
-int	main(void)
+//throw an exception if user tries to add int above max amount
+void	intOverflow(void)
 {
-	// Span sp = Span(5);
-	// sp.addNumber(6);
-	// sp.addNumber(3);
-	// sp.addNumber(17);
-	// sp.addNumber(9);
-	// sp.addNumber(11);
-	// std::cout << sp.shortestSpan() << std::endl;
-	// std::cout << sp.longestSpan() << std::endl;
-
-	// try
-	// {
-	// 	Span	vec(5);
-
-	// 	vec.addNumber(11);
-	// 	vec.addNumber(-100);
-	// 	vec.addNumber(20);
-	// 	vec.addNumber(50);
-	// 	vec.addNumber(40);
-	// 	// vec.addNumber(100);
-		
-	// 	std::cout << "index 2: " << vec[2] << std::endl;
-
-	// 	std::cout << "longest span: " << vec.longestSpan() << std::endl;
-	// 	std::cout << "shortest span: " << vec.shortestSpan() << std::endl;
-
-	// 	std::cout << "this should throw an exception: " << vec[-1] << std::endl;
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cout << e.what() << '\n';
-	// }
-
-	// try
-	// {
-	// 	Span	vec2(10000);
-
-	// 	for (int i = 0; i != 10000; i++)
-	// 		vec2.addNumber(i * 15 % 100000);
-		
-	// 	std::cout << "longest span: " << vec2.longestSpan() << std::endl;
-	// 	std::cout << "shortest span: " << vec2.shortestSpan() << std::endl;
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cout << e.what() << '\n';
-	// }
-	
-	//adding numbers using a range of iterators
-
-	Span			span(10);
-
-	span.addNumber(1);
-	span.addNumber(2);
-
-	std::list<int>	intList(10);
-	intList.push_back(3);
-	intList.push_back(4);
-	intList.push_back(5);
-	intList.push_back(6);
+	Span sp = Span(3);
 
 	try
 	{
-		span.addNumber(intList.begin(), intList.end());
-		span.printSpan();
+		sp.addNumber(0);
+		sp.addNumber(0);
+		sp.addNumber(0);
+		sp.addNumber(0);
 	}
 	catch(const std::exception& e)
 	{
-		std::cerr << e.what() << "Range of iterators exceeding max _N" << '\n';
+		std::cerr << e.what() << " 4th int is too much" << '\n';
 	}
-	
+}
+
+//only zero or one number in span
+void	noSpanPossible(void)
+{
+	Span sp = Span(10);
+
+	try
+	{
+		sp.shortestSpan();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << " no span because no number" << '\n';
+	}
+	try
+	{
+		sp.addNumber(200);
+		sp.longestSpan();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << " no span because one number" << '\n';
+	}
+}
+
+void	randomNumbers(void)
+{
+	Span sp(20000);
+	srand(time(0));
+	for (int i = 0; i != 20000; i++)
+		sp.addNumber(rand());
+
+	std::cout << "longest span: " << sp.longestSpan() << '\n';
+	std::cout << "shortest span: " << sp.shortestSpan() << '\n';
+	std::cout << "value of 10000th entry: " << sp[10000] << '\n';
+
+	// run ./span | wc -l to verify all 15000 are stored
+	// sp.printSpan();
+}
+
+void	fillWithIterators(void)
+{
+	Span sp(10);
+
+	std::list<int>	intList;
+	intList.push_back(50);
+	intList.push_back(100);
+	intList.push_back(200);
+
+	try
+	{
+		sp.addNumber(intList.begin(), intList.end());
+		sp.printSpan();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << " list of iterators bigger than N" << '\n';
+	}
+}
+
+int	main(void)
+{
+	std::cout << "example from subject: " << std::endl;
+
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+
+	intOverflow();
+	noSpanPossible();
+	randomNumbers();
+	fillWithIterators();
 }
