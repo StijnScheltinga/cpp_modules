dae59c97e2497c221d05712a80a6d1621f1e28d8
StijnScheltinga <stijnskoop@gmail.com>
2024-08-23 15:40:37 +0200

    date and value checks

--- cpp_module_09/ex00/BitcoinExchange.cpp
+++ cpp_module_09/ex00/BitcoinExchange.cpp
@@ -28,7 +28,84 @@ void	BitcoinExchange::parseDatabase()
 			continue;
 		exchangeRate = line.substr(line.find_first_of(",") + 1, std::string::npos);
 		btcPrices[date] = stod(exchangeRate);
-		printDatabase();
+		// printDatabase();
+	}
+	parseInput();
+}
+
+void	BitcoinExchange::parseInput()
+{
+	std::string	line;
+	std::string	date;
+	std::string	value;
+	std::size_t	middlePos;
+
+	while(getline(input, line))
+	{
+		middlePos = line.find_first_of("|");
+		if (middlePos == std::string::npos)
+		{
+			std::cout << "middle is not found" << "\n";
+			continue;
+		}
+		//need to implement check to know if it is possible to substr date and value
+		date = line.substr(0, middlePos - 1);
+		if (date == "date")
+			continue;
+		value = line.substr(middlePos + 2, std::string::npos);
+
+		try
+		{
+			dateCheck(date);
+			valueCheck(value);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << line << '\n';
+		}
+			
+		std::cout << "date: " << date << ", value: " << value << '\n';
+
+	}
+}
+
+void	BitcoinExchange::valueCheck(std::string value)
+{
+	if (value.find_first_of(".") != std::string::npos)
+	{
+		try
+		{
+			stof(value);
+		}
+		catch(const std::exception& e)
+		{
+			throw ValueFormatException();
+		}
+	}
+	else
+	{
+		try
+		{
+			stoi(value);
+		}
+		catch(const std::exception& e)
+		{
+			throw ValueFormatException();
+		}
+	}
+
+}
+
+void	BitcoinExchange::dateCheck(std::string date)
+{
+	if (date.size() != 10 || date[4] != '-' || date[7] != '-')
+		throw BitcoinExchange::DateFormatException();
+	for (std::size_t i = 0; i != date.size(); i++)
+	{
+		if (i == 4 || i == 7)
+			continue;
+		if (!isdigit(date[i]))
+			throw BitcoinExchange::DateFormatException();
 	}
 }
 
--- cpp_module_09/ex00/BitcoinExchange.hpp
+++ cpp_module_09/ex00/BitcoinExchange.hpp
@@ -13,10 +13,30 @@ class BitcoinExchange
 		std::map<std::string, double>	btcPrices;
 
 		void	parseDatabase();
+		void	parseInput();
+		void	dateCheck(std::string date);
+		void	valueCheck(std::string value);
 		void	printDatabase();
+
+		class DateFormatException : public std::exception
+		{
+			public:
+				const char* what() const noexcept {
+					return "Incorrect date format! => ";
+				}
+		};
+		
+		class ValueFormatException : public std::exception
+		{
+			public:
+				const char* what() const noexcept {
+					return "Incorrect value format! => ";
+				}
+		};
 	public:
 		BitcoinExchange(const std::string& iFile, const std::string& dbFile);
 		~BitcoinExchange();
 };
+
 
 #endif
